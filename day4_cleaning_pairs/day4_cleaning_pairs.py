dd1cc59eab6d9c9279d8d817b1e28eb9c069099b
anna-savina <asavina@redhat.com>
2022-12-04 10:03:59 +0200

    refactoring day4refactoring day4refactoring day4refactoring day4

--- day4_cleaning_pairs/day4_cleaning_pairs.py
+++ day4_cleaning_pairs/day4_cleaning_pairs.py
@@ -15,17 +15,26 @@ def absolute_file_name(file_name):
     return os.path.join(os.path.dirname(__file__), file_name)
 
 
+def convert_data_to_int(line):
+    groups = line.split(',')
+    group1 = groups[0].split('-')
+    group2 = groups[1].split('-')
+    for i in range(2):
+        group1[i] = int(group1[i])
+        group2[i] = int(group2[i])
+    
+    return [group1, group2]
+
+
 def calculate_overlap_part1(file_name):
     with open(absolute_file_name(file_name)) as f:
         res = 0
         for line in f:
             line = line.strip('\n')
-            groups = line.split(',')
-            group1 = groups[0].split('-')
-            group2 = groups[1].split('-')
-            if int(group1[0]) <= int(group2[0]) and int(group1[-1]) >= int(group2[-1]):
+            groups = convert_data_to_int(line)
+            if groups[0][0] <= groups[1][0] and groups[0][-1] >= groups[1][-1]:
                 res += 1
-            elif int(group2[0]) <= int(group1[0]) and int(group2[-1]) >= int(group1[-1]):
+            elif groups[1][0] <= groups[0][0] and groups[1][-1] >= groups[0][-1]:
                 res += 1
     return res
 
@@ -35,16 +44,8 @@ def calculate_overlap_part2(file_name):
         res = 0
         for line in f:
             line = line.strip('\n')
-            groups = line.split(',')
-            group1 = groups[0].split('-')
-            group2 = groups[1].split('-')
-            if int(group1[0]) <= int(group2[0]) and int(group2[0]) <= int(group1[-1]):
-                res += 1
-            elif int(group2[-1]) >= int(group1[0]) and int(group2[-1]) <= int(group1[-1]):
-                res += 1
-            elif int(group1[0]) >= int(group2[0]) and int(group1[0]) <= int(group2[-1]):
-                res += 1
-            elif int(group1[-1]) >= int(group2[0]) and int(group1[-1]) <= int(group2[-1]):
+            groups = convert_data_to_int(line)
+            if range(max(groups[1][0], groups[0][0]), min(groups[1][-1], groups[0][-1])+1):
                 res += 1
     return res
 
